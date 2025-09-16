4e6ce2fffbc0ad9364bec7b03da07a2cf0c83aeb
Karsten <info@weng.eu>
2025-09-16 14:02:51 +0200

    add READ.me and update postcss.config

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,19 @@
+/**
+ * PostCSS-Konfiguration
+ *
+ * PostCSS ist ein „Nachbearbeiter“ für CSS, das das Plugin **Autoprefixer** nutzt.
+ * Es fügt automatisch Vendor-Prefixes (z. B. -webkit-, -ms-) für ältere Browser hinzu.
+ *
+ * Ablauf:
+ *   1) Sass kompiliert SCSS → CSS (npm run build:sass / npm run dev)
+ *   2) PostCSS läuft über erzeugte CSS (npm run build:css) und fügt Prefixes hinzu
+ *
+ * Unterstützte Browser: siehe „browserslist“ in der package.json (siehe unten).
+ */
+
 module.exports = {
-    plugins: [require("autoprefixer")],
+    // Liste der PostCSS-Plugins, die auf CSS angewendet werden
+    plugins: [
+        require("autoprefixer"), // fügt fehlende Vendor-Prefixes automatisch ein
+    ],
 };
